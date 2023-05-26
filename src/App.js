866914a89ba0aa5ab93dc85531fd04cf65c8eb49
Luciano <luciano@standupx.com>
2023-05-26 18:26:56 +0200

    Add slider gsap react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+//import "reset.css";
+import "./App.scss";
+import leftArrow from './assets/images/arrow-left.svg';
+import rightArrow from './assets/images/arrow-right.svg';
+import testimonials from './data/testimonialsData';
 
-function App() {
+const App = () => {
+let imageList = useRef(null);
+let testimonialList = useRef(null);
+
+useEffect(()=> {
+  console.log(imageList)
+})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='testimonial-section'>
+      <div className="testimonial-container">
+        <div className='arrows left'>
+          <span>
+            <img src={leftArrow} alt='left arrow' />
+          </span>
+        </div>
+
+        <div className="inner">
+          <div className="t-image">
+            <ul ref={el =>(imageList = el)}>
+              {testimonials.map((testimonial) => (
+                <li key={testimonial.id}>
+                  <img src={testimonial.image} alt={testimonial.name} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="t-content">
+          <ul ref={el =>(imageList = el)}>
+              {testimonials.map((testimonial) => (
+                <li key={testimonial.id}>
+                  <div className="content-inner">
+                    <p className='quote'>{testimonial.quote}</p>
+                    <h3 className='name'>{testimonial.name}</h3>
+                    <h4 className='title'>{testimonial.title}</h4>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className='arrows right'>
+          <span>
+            <img src={rightArrow} alt='right arrow' />
+          </span>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,30 @@
+import image1 from "../assets/images/image3.jpg";
+import image2 from "../assets/images/image.jpg";
+import image3 from "../assets/images/image2.jpg";
+
+
+const testimonials = [
+  {
+    id: 1,
+    name: "Julia Cameron",
+    title: "creative director",
+    image: image1,
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget justo pharetra."
+  },
+  {
+    id: 2,
+    name: "Lucio Ticali",
+    title: "Senior developer",
+    image: image2,
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget justo pharetra."
+  },
+  {
+    id: 3,
+    name: "Lisa Rossi",
+    title: "Brand coordinator",
+    image: image3,
+    quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget justo pharetra."
+  }
+];
+
+export default testimonials;
